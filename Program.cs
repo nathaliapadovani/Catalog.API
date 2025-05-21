30e0e5683de590ff8c5f09c194851bbbbb14d61c
Nathalia <nathaliapadovani.contato@gmail.com>
2025-05-21 16:53:57 -0300

    Adiciona configurações de serviços, Swagger e pipeline no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
+using Catalog.API.Data; // Namespace para classes relacionadas ao acesso a dados
+using Catalog.API.Repositories; // Namespace para os repositórios
+using Microsoft.OpenApi.Models; // Fornece classes para configurar o Swagger
+
+// Base para configurar serviços e a aplicação
+// Inicia a configuração da API, substituindo a abordagem do Startup.cs
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Adiciona serviços ao contêiner
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+});
 
 var app = builder.Build();
 
