d88f7b40f60576e3ef026117338aa0dff40ba908
Nathalia <nathaliapadovani.contato@gmail.com>
2025-05-21 21:21:05 -0300

    fix: Remove arquivos temporários do repositório

---
+++ Controllers/ArgmentNullException.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Catalog.API.Controllers
+{
+    [Serializable]
+    internal class ArgmentNullException : Exception
+    {
+        public ArgmentNullException()
+        {
+        }
+
+        public ArgmentNullException(string? message) : base(message)
+        {
+        }
+
+        public ArgmentNullException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,46 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+
+    // ControllerBase -> BadRequest(), NotFound(), Ok(), TryUpdateModelAsync(), TryValidateModel()
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public CatalogController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        // GET: api/v1/catalog
+        // Obtem todos os produtos
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+        }
+
+        // GET: api/v1/catalog/{id}
+        // Obtem um produto pelo id
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _repository.GetProduct(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
