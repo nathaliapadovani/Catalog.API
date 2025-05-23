72be17b0714fc0f2c25acbf1bc7b3d32eafd92a4
Nathalia <nathaliapadovani.contato@gmail.com>
2025-05-23 17:10:30 -0300

    Inclusão do CatalogController.cs

--- Controllers/CatalogController.cs
+++ Controllers/CatalogController.cs
@@ -13,6 +13,7 @@ namespace Catalog.API.Controllers
     {
         private readonly IProductRepository _repository;
 
+        // Construtor que recebe a interface IProductRepository via injeção de dependência
         public CatalogController(IProductRepository repository)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
@@ -42,5 +43,59 @@ namespace Catalog.API.Controllers
             }
             return Ok(product);
         }
+
+        // GET: api/v1/catalog/{category}
+        // Obtém produtos por categoria
+        [Route("[action]/{category}", Name = "GetpductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Product>))]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            if (category is null)
+                return BadRequest("Invalid category");
+
+                var products = await _repository.GetProductByCategory(category);
+
+            return Ok(products);
+        }
+
+        // POST: api/v1/catalog
+        // Cria um novo produto
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            if (product is null)
+                return BadRequest("Invalid product");
+
+            await _repository.CreateProduct(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        // PUT: api/v1/catalog
+        // Atualiza um produto existente
+        [HttpPut]
+        [ProducesResponseType(typeof (Product), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            if (product is null)
+                return BadRequest("Invalid product");
+
+            return Ok(await _repository.UpdateProduct(product));
+        }
+
+        // DELETE: api/v1/catalog/{id}
+        // Deleta um produto existente, pelo id
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteProductById(string id)
+        {
+            return Ok(await _repository.DeleteProduct(id));
+        }
+
     }
 }
